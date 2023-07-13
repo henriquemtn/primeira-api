44ca6ea63998d3a85262c68eefdae1211f8463eb
henriquemtn <heniquemartinsilveira2@gmail.com>
2023-07-13 01:10:09 -0300

    first commit

---
+++ src/main/java/br/projeto/api/repositorio/Repositorio.java
@@ -0,0 +1,35 @@
+package br.projeto.api.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.projeto.api.modelo.Pessoa;
+
+@Repository
+public interface Repositorio extends CrudRepository<Pessoa, Integer> {
+    
+    List<Pessoa> findAll();
+
+    Pessoa findByCodigo(int codigo);
+
+    List<Pessoa> findByOrderByNomeDesc();
+
+    List<Pessoa> findByNomeOrderByIdadeDesc(String nome);
+
+    List<Pessoa> findByNomeContaining(String termo);
+
+    List<Pessoa> findByNomeStartsWith(String termo);
+
+    List<Pessoa> findByNomeEndingWith(String termo);
+
+    @Query(value = "SELECT SUM(idade) FROM pessoas", nativeQuery = true)
+    int somaIdades();
+
+    @Query(value = "SELECT * FROM pessoas WHERE idade >= :idade", nativeQuery = true)
+    List<Pessoa> idadeMaiorIgual(int idade);
+
+    int countByCodigo(int codigo);
+} 
